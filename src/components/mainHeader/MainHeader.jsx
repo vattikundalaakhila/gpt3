796e6906ceb4c34c1e57ba390f00d7ccf1fa6f9a
vattikundalaakhila <akhila.vattikundala.0511@gmail.com>
2025-12-16 18:29:42 +0530

    starting new project

---
+++ src/components/mainHeader/MainHeader.jsx
@@ -0,0 +1,17 @@
+const mainHeader = () => {
+    return (
+        <header className="main_header">
+            <div className="main_header_logo">
+
+            </div>
+            <nav className="main_header_navlinks">
+
+            </nav>
+            <div className="main_header_button">
+
+            </div>
+
+        </header>
+    )
+}
+export default mainHeader;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const home = () => {
+    return (
+        <>
+        <br />
+     this is home where home components will go 
+     </>
+    );
+
+}
+export default home; 
