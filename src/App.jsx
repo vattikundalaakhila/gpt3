aa5d68b291592cc5abe4b5227ffac2d486b50270
vattikundalaakhila <akhila.vattikundala.0511@gmail.com>
2025-12-18 17:40:56 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import MainHeader from './components/MainHeader/MainHeader'
 const App = () => {
   return (
     <div className='App'>
-
+      <MainHeader/>
     </div>
   )
 }
