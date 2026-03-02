8ee1e9c2f422f53ff70f4c33744eb042646ce3d9
vattikundalaakhila <akhila.vattikundala.0511@gmail.com>
2026-03-02 16:04:43 +0530

    Updated README and project styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -12,45 +12,48 @@ import people_img6 from "../../assets/Ellipse 34.png"
 const Banner = () => {
   return (
     <>
-    <section className="banner_section layout">
-      <div className="banner_section_left">
-        <h1 className="gradient_text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
-        <p>
-          Yet bed any for travelling assistance indulgence
-          unpleasing. Not thoughts all exercise blessing.
-          Indulgence way everything joy alteration
-          boisterous the attachment. Party we years to order
-          allow asked of. </p>
-        <div>
-          <input type="email"
-            name="email"
-            id="email"
-            placeholder="Your Email Address"
-          />
-          <button className="pry_btn active">Get Started</button>
+      <section className="banner_section layout">
+        <div className="banner_section_left">
+          <h1 className="gradient_text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+          <p>
+            Yet bed any for travelling assistance indulgence
+            unpleasing. Not thoughts all exercise blessing.
+            Indulgence way everything joy alteration
+            boisterous the attachment. Party we years to order
+            allow asked of. </p>
+          <div className="banner_input_box">
+            <input type="email"
+              name="email"
+              id="email"
+              placeholder="Your Email Address"
+            />
+            <button className="pry_btn active">Get Started</button>
 
+          </div>
+
+          <div className="banner_section_bottom">
+            <div>
+            <img src={people_img} alt="person" />
+            <img src={people_img2} alt="person" />
+            <img src={people_img3} alt="person" />
+            <img src={people_img4} alt="person" />
+            <img src={people_img5} alt="person" />
+            <img src={people_img6} alt="person" />
+
+            <p className="p_img">1.6k</p>
+            </div>
+<div>
+            <p className="p_tag">1,600 people requested access a visit in last 24 hours. </p>
+
+</div>
+
+          </div>
         </div>
-
-    <div className="banner_section_bottom">
-      <img src= {people_img} alt="person"/>
-      <img src= {people_img2} alt="person"/>
-      <img src= {people_img3} alt="person"/>
-      <img src= {people_img4} alt="person"/>
-      <img src= {people_img5} alt="person"/>
-      <img src= {people_img6} alt="person"/>
-      
-      <p className="p_img">1.6k</p>
-      <p className="p_tag">1,600 people requested access a visit in last 24 hours. </p>
-      
-
-
-    </div>
-      </div>
-      <div className="banner_section_right">
-        <img src={bg_img} alt="bg_img" /></div>
-    </section>
+        <div className="banner_section_right">
+          <img src={bg_img} alt="bg_img" /></div>
+      </section>
     </>
-  
+
   )
 }
 
